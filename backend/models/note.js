1badf8799fd372ce5def061a0f29a20b9f142bcb
adolp26 <adolpheribeiro7@gmail.com>
2024-06-01 20:29:49 -0300

    feat: create models

---
+++ backend/models/note.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+    const Note = sequelize.define('Note', {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+    });
+    return Note;
+  };
+  
