ce50a72e3ef5c1415a04db611ec3a7558e2cde5a
adolp26 <adolpheribeiro7@gmail.com>
2024-06-05 13:03:31 -0300

    test

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Funcionando meu parceiro, finalmente!!!');
+  res.send('Funcionando!!!');
 });
 
 const port = 3001;
